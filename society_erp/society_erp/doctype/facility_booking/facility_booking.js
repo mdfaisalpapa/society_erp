57f3d0e20d25791e691f8d275f55ee7bbee3cac9
Mohammed Faisal <mdfaisalpapa@gmail.com>
2026-08-12 04:10:58 +0000

    Initial commit: Added custom DocTypes and Custom Field fixtures

---
+++ society_erp/society_erp/doctype/facility_booking/facility_booking.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Mohammed Faisal and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Facility Booking", {
+// 	refresh(frm) {
+
+// 	},
+// });
